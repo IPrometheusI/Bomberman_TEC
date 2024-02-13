7f7d4677c28f6fc18569b268085e83c8365bab1a
Michael Fernandez <unknownhuman@estudiantec.cr>
2024-02-12 22:29:36 -0600

    Se crea una ventana de 1280x720

--- BBMAN.c
+++ BBMAN.c
@@ -2,29 +2,37 @@
 
 //@Author: Maickol Fernandez
 
-//Using libs SDL, glibc
-#include <SDL.h>	//SDL version 2.0
-#include <stdlib.h>
+#include "SDL.h"   /* Se incluye la libreria SDL */
 #include <stdio.h>
-
-
-// Numbers bitmap, some routines and and SDL initialization taken from
-// https://github.com/flightcrank/pong
-
-
-// Please follow the coding guidelines described in:
-// https://users.ece.cmu.edu/~eno/coding/CCodingStandard.html
+#include <unistd.h>
 
 
 
-/* Do not use magic numbers in the code, define the meaning of the number
-   in this section and the use the define across the code. This increases
-   readability accross the code
-*/
 
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   printf("push desde consola")
-   return 0;
+
+    SDL_Surface *screen;
+
+    /* Initialize the SDL library */
+    if( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+        fprintf(stderr,
+                "Couldn't initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    /* Clean up on exit */
+    atexit(SDL_Quit);
+    
+    /*
+     * Initialize the display in a 1280x720 8-bit palettized mode,
+     * requesting a software surface
+     */
+    screen = SDL_SetVideoMode(1280,720 , 8, SDL_SWSURFACE);
+
+    sleep(2); // format is Sleep(x); where x is # of seconds.
+    
+    if ( screen == NULL ) {
+        fprintf(stderr, "Couldn't set 640x480x8 video mode: %s\n",
+                        SDL_GetError());
+        exit(1);}
 }
